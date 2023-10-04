cc639c98cfd95545cfd041e0950658fce163c170
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-04 10:28:28 -0600

    Variables - First Commit

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,3 +1,27 @@
 fun main(args: Array<String>) {
     println("Hello World")
+    println("My first Kotlin program")
+
+    var tim: String
+    tim = "Tim Buchalka"
+    println(tim)
+
+    var timWeeklySalary: Int = 32
+    var timMonthlySalary: Int = timWeeklySalary * 4
+    println(timWeeklySalary)
+    println(timMonthlySalary)
+
+    println()
+
+    val apples: Int = 6
+    val oranges: Int = 5
+    var fruit: Int = apples - oranges
+    println(fruit)
+
+    println(apples / 4)
+
+    println()
+    val weeks: Int = 130
+    val years: Double = weeks / 52.0
+    println(years)
 }
