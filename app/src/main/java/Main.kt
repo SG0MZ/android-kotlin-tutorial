eb18fc406447ff1606194b33e39cc4dfa1b64568
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-04 10:54:49 -0600

    Declarations - First Commit

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -4,29 +4,29 @@ fun main(args: Array<String>) {
 
     println()
 
-    var tim: String
-    tim = "Tim Buchalka"
+    val jim = "James T Kirk"
+    val tim = "Tim Buchalka"
     println("My name is $tim")
     println("I can print \$tim")
 
     println()
 
-    var timWeeklySalary: Int = 32
-    var timMonthlySalary: Int = timWeeklySalary * 4
+    val timWeeklySalary = 32
+    val timMonthlySalary = timWeeklySalary * 4
     println("Tim's $ weekly salary is $$timWeeklySalary")
     println("Monthly, that comes to $timMonthlySalary")
 
     println()
 
-    val apples: Int = 6
-    val oranges: Int = 5
-    var fruit: Int = apples - oranges
+    val apples = 6
+    val oranges = 5
+    val fruit = apples - oranges
     println("$apples apples - $oranges oranges leaves $fruit piece(s) of fruit")
 
     println("A quarter of the apples is ${apples / 4}")
 
     println()
-    val weeks: Int = 130
-    val years: Double = weeks / 52.0
+    val weeks = 130
+    val years = weeks / 52.0
     println("$weeks weeks is $years years")
 }
