fa8bc1a9ce82f532dab00c3786be3e0534a43a15
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-04 11:18:05 -0600

    Classes - First Commit

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,33 +1,17 @@
 fun main(args: Array<String>) {
-    val lives = 0
 
-    var isGameOver = (lives < 1)
-    println(isGameOver)
+    val tim = Player("Tim")
+//    println(tim.name)
+//    println(tim.lives)
+//    println(tim.level)
+//    println(tim.score)
+    tim.show()
 
-    if (isGameOver) {
-        println("Game Over!")
-    } else {
-        println("You're still alive!")
-    }
+    val louise = Player("Louise",5)
+    louise.show()
 
-    println("How old are you: ")
-    val age = readLine()!!.toInt()
-    println("age is $age")
-
-    val message: String
-    message = when {
-        age < 18 -> "You're too young to vote"
-        age == 100 -> "Congratulations"
-        else -> "You can vote"
-    }
-
-//    message = if(age < 18) {
-//        "You're too young to vote"
-//    } else if(age == 100) {
-//        "Congratulations"
-//    } else {
-//        "You can vote"
-//    }
-
-    println(message)
+    val gr8 = Player("gr8", 4, 8)
+    val one2watch = Player("Ace",2,5,1000)
+    gr8.show()
+    one2watch.show()
 }
