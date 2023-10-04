d7fc65c16b56f5545ca5082d7e86e2a408221d11
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-04 10:48:10 -0600

    Concatenation, Interpolation - First Commit

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -2,26 +2,31 @@ fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
 
+    println()
+
     var tim: String
     tim = "Tim Buchalka"
-    println(tim)
+    println("My name is $tim")
+    println("I can print \$tim")
+
+    println()
 
     var timWeeklySalary: Int = 32
     var timMonthlySalary: Int = timWeeklySalary * 4
-    println(timWeeklySalary)
-    println(timMonthlySalary)
+    println("Tim's $ weekly salary is $$timWeeklySalary")
+    println("Monthly, that comes to $timMonthlySalary")
 
     println()
 
     val apples: Int = 6
     val oranges: Int = 5
     var fruit: Int = apples - oranges
-    println(fruit)
+    println("$apples apples - $oranges oranges leaves $fruit piece(s) of fruit")
 
-    println(apples / 4)
+    println("A quarter of the apples is ${apples / 4}")
 
     println()
     val weeks: Int = 130
     val years: Double = weeks / 52.0
-    println(years)
+    println("$weeks weeks is $years years")
 }
